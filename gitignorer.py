59e23d45e9aa4cb73df0e63cfcc665355eb0868f
idotavorslab <48498791+idotavorslab@users.noreply.github.com>
2019-03-13 02:11:08 +0200

    out of gitignore: "pyano_local_modules midiconvert jquery electron-store electron-forge electron @tonejs python-shell sweetalert2 tone webmidi

--- gitignorer.py
+++ gitignorer.py
@@ -3,10 +3,13 @@ from optparse import OptionParser
 
 parser = OptionParser()
 parser.add_option("-r", "--root_dir", help="path of the project's root directory, where `node_modules` and `.gitignore` lay")
-parser.add_option("-k", "--keep", help="what folder to keep. example: `pyano_local_modules`")
+parser.add_option("-k", "--keep", help="what folder(s) to keep. can be space separated. example: `pyano_local_modules` or `foo bar baz`")
 (options, args) = parser.parse_args()
+
+keep = options.keep.split()
+print(f'keeping outside of gitignore: {keep}')
 node_modules_path = os.path.join(options.root_dir, "node_modules")
 gitignore_path = os.path.join(options.root_dir, ".gitignore")
 with open(gitignore_path, mode='a') as gitignore:
     [gitignore.write('node_modules/' + module + '\n')
-     for module in os.listdir(node_modules_path) if options.keep not in module]
+     for module in os.listdir(node_modules_path) if module not in keep]
