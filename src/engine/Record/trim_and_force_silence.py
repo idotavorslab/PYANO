fbf332bd6b42c402bde96968056033ef351793e8
GbeTech <gbetech@gmail.com>
2019-03-17 20:30:03 +0200

    record page now trims vide by notes

--- src/engine/Record/trim_and_force_silence.py
+++ src/engine/Record/trim_and_force_silence.py
@@ -40,11 +40,17 @@ vid = sys.argv[1]
 first_onset = float(sys.argv[2])
 silence_len = int(sys.argv[3])
 no_ext = os.path.splitext(vid)[0]
+
+logged_exp = dict(vid=vid, first_onset=first_onset,
+                  silence_len=silence_len, no_ext=no_ext)
 try:
     to_secs = float(sys.argv[4])
     to_str = get_hhmmss(to_secs)
     notes_num = int(sys.argv[5])
-    output = f'{no_ext}_trimmed_{notes_num}N.mp4'
+    output = f'{no_ext}_{notes_num}N.mp4'
+
+    logged_exp.update(to_secs=to_secs, to_str=to_str,
+                      notes_num=notes_num, output=output)
 except IndexError:
     dur = float(os.popen(f'ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {vid}')
                 .read()
@@ -53,23 +59,25 @@ except IndexError:
     # dur = librosa.get_duration(filename=vid)
     to_str = get_hhmmss(dur)
     output = f'{no_ext}_trimmed.mp4'
+
+    logged_exp.update(dur=dur, to_str=to_str, output=output)
+
+if os.path.exists(output):
+    logger.log(logged_exp, title=f"FileExistsError - {output}. see trim_and_force_silence.log")
+    raise FileExistsError(output)
 start_secs = float(first_onset - silence_len)
+logged_exp.update(start_secs=start_secs)
 if start_secs < 0:
-    raise ValueError("""First onset minus silence length is negative. 
+    err = """First onset minus silence length is negative. 
     This means first onset was early, and silence length was too large. 
-    You are always better off starting to record vid way before first note. """)
+    You are always better off starting to record vid way before first note. see trim_and_force_silence.log"""
+    logger.log(logged_exp, title=err)
+    raise ValueError(err)
 
 start_str = get_hhmmss(start_secs)
 
 ok = os.system(f'ffmpeg -ss {start_str} -i "{vid}" -c copy -t {to_str} "{output}"')
 if ok != 0:
-    raise ValueError("ok system ffmpeg -ss blah blah copy output failed")
-logged_exp = dict(vid=vid, first_onset=first_onset, silence_len=silence_len,
-                  no_ext=no_ext, output=output, to_str=to_str,
-                  start_secs=start_secs, start_str=start_str, ok=ok)
-if len(sys.argv) > 4:
-    logged_exp.update(to_secs=to_secs, notes_num=notes_num)
-else:
-    logged_exp.update(dur=dur)
-logger.log(logged_exp)
+    logger.log(logged_exp, title="ValueError - ok is not 0")
+    raise ValueError("ok system ffmpeg -ss blah blah copy output failed. see trim_and_force_silence.log")
 print(output)
