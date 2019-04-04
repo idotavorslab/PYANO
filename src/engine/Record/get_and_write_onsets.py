727fcfbb499c4575e475c36ebacb1469f3299d2c
GbeTech <gbetech@gmail.com>
2019-04-04 16:18:09 +0300

    Update get_and_write_onsets.py

--- src/engine/Record/get_and_write_onsets.py
+++ src/engine/Record/get_and_write_onsets.py
@@ -2,6 +2,7 @@ import librosa
 import os
 import sys
 import json
+from util import Logger
 
 vid = sys.argv[1]
 y, sr = librosa.load(vid, sr=22050)
@@ -12,6 +13,8 @@ onsets = librosa.onset.onset_detect(y=y,
 onset_times = librosa.frames_to_time(onsets,
                                      sr=sr,
                                      hop_length=hop_length)
+logger = Logger('get_and_write_onsets')
+logger.log_thin(dict(onsets=onsets, onset_timers=onset_times))
 no_ext, _ = os.path.splitext(vid)
 with open(f'{no_ext}_onsets.json', mode='w+') as f:
     json.dump(dict(onsets=[str(t) for t in onset_times]), f)
