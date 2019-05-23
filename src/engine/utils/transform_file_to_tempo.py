5699305fb4bdb4ec585cfba372ddc3d10b1e73c7
Gilad Barnea <giladbrn@gmail.com>
2019-05-23 19:00:41 +0300

    can now set tempo up to 300%

--- src/engine/utils/transform_file_to_tempo.py
+++ src/engine/utils/transform_file_to_tempo.py
@@ -1,8 +1,8 @@
 from classes import Message
 
-on_file = r'c:\Sync\Code\Python\Pyano-release\src\experiments\truths\magnet_prelude_8s_on.txt'
-output_path = r'c:\Sync\Code\Python\Pyano-release\src\experiments\truths\magnet_prelude_7500ms_on.txt'
-accelerate_factor = 0.9375
+on_file = r'c:\Sync\Code\Python\Pyano-release\src\experiments\truths\fur_elise_b_14000ms_on.txt'
+output_path = r'c:\Sync\Code\Python\Pyano-release\src\experiments\truths\fur_elise_b_13500ms_on.txt'
+accelerate_factor = 13.5 / 14  # <1 for shorter duration (faster)
 msgs = Message.construct_many_from_file(on_file)
 
 tempoed = Message.transform_to_tempo(msgs, accelerate_factor * 100)
