380322f9239f7cedc6e7d01bc466d400ad3c39f0
Gilad Barnea <giladbrn@gmail.com>
2020-05-28 16:20:23 +0300

    bhe 1.1.3

--- src/foo.js
+++ src/foo.js
@@ -1,5 +1,4 @@
+const BHE = require("betterhtmlelement/dist/bhe");
+
 console.log('foo.js');
-const BHE = require('betterhtmlelement/dist/bhe');
-console.log('BHE:', BHE);
-const e = BHE.input();
-console.log('e:', e);
+console.log(BHE);
